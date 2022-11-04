67deb10936f2ed6ed45570b58e200b1d6a50ea42
Julian <al_jdomenecht@iessierradelasvillas.org>
2022-11-04 11:08:16 +0100

    Hacer el 12

---
+++ Ejercicio11.py
@@ -0,0 +1,12 @@
+
+num_primo=0
+numero=int(input("Dime un número"))
+for i in range (2,numero):
+    if numero%i==0:
+        num_primo+=1
+if num_primo>=1:
+    print("El número no es primo")
+else:
+    print("El número es primo")
+    
+
--- Ejercicio9.py
+++ Ejercicio9.py
@@ -1,10 +1,8 @@
-salida=-1
+
 base=int(input("Dime la base de la potencia"))
 poten=base
 expo=int(input("Dime el exponente de la potencia"))
 
-for i in range(expo):
-    poten=poten*base
-
-
-print(base)
+for i in range(expo-1):
+    poten*= base
+print(poten)
