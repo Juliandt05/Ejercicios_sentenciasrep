ebbf0c3cb0b055a1ebc9329639beddb8f9c682e1
Julian <al_jdomenecht@iessierradelasvillas.org>
2022-11-07 11:11:09 +0100

    Hacer el ej 15

---
+++ Ejercicio13.py
@@ -0,0 +1,6 @@
+horastot=0
+for x in range (1,7):
+    horas_dia=int(input("Dime las horas que has trabajado en el dia: "))
+    horastot=horastot+horas_dia
+sueldohora=int(input("Dime lo que cobras por hora"))
+print("Tu sueldo total es de",sueldohora*horastot)
---
+++ Ejercicio14.py
@@ -0,0 +1,6 @@
+Kilom1=70
+Kilom2=150
+while Kilom1!=Kilom2:
+    Kilom1+=1
+    Kilom2-=1
+print("Se encuentran en el Kilómetro",Kilom2)
