09ff2717c99047e97f2f4aff8a115c9461781208
Julian <al_jdomenecht@iessierradelasvillas.org>
2022-11-21 11:13:50 +0100

    Tkinter

--- Tkinter.py
+++ Tkinter.py
@@ -5,6 +5,7 @@ from tkinter import ttk
 def saludar():
     texto=campoTexto.get()
     print(texto)
+    textoLabel.set(texto)
 
 
 #Generar la ventana
@@ -12,12 +13,14 @@ ventana = Tk()
 ventana.title("Primer ejercicio")
 ventana.geometry("300x100")
 ventana.resizable(width=False,height=False)
-ventana.config(background="Blue")
+ventana.config(background="Light Blue")
 #Generar el lienzo para pintar componentes
 frm = ttk.Frame(ventana, padding=10).pack()
 #Componentes Label y button
-labelTexto = ttk.Label(frm, text = "Hola Tkinter") 
-labelTexto.config(background="red",border=5,font=("Arial",15))
+textoLabel=StringVar()
+textoLabel.set("Hola Tkinter")
+labelTexto = ttk.Label(frm, textvariable=textoLabel) 
+labelTexto.config(background="blue",border=5,font=("Arial",15))
 labelTexto.pack()
 ttk.Button(frm, text="Salir", command=ventana.destroy).pack()
 
